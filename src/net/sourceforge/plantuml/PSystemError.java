8db8020b28bd53082c8c7ed97533889707315372
David Benson <david@jgraph.com>
2018-12-05 13:50:08 +0000

    Removed patreon messages

--- src/net/sourceforge/plantuml/PSystemError.java
+++ src/net/sourceforge/plantuml/PSystemError.java
@@ -7,10 +7,7 @@
  * Project Info:  http://plantuml.com
  * 
  * If you like this project or if you find it useful, you can support us at:
- * 
- * http://plantuml.com/patreon (only 1$ per month!)
- * http://plantuml.com/paypal
- * 
+ *
  * This file is part of PlantUML.
  *
  * PlantUML is free software; you can redistribute it and/or modify it
@@ -34,10 +31,6 @@
  */
 package net.sourceforge.plantuml;
 
-import java.awt.Color;
-import java.awt.geom.Dimension2D;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
@@ -54,34 +47,16 @@ import net.sourceforge.plantuml.asciiart.UmlCharArea;
 import net.sourceforge.plantuml.core.DiagramDescription;
 import net.sourceforge.plantuml.core.ImageData;
 import net.sourceforge.plantuml.core.UmlSource;
-import net.sourceforge.plantuml.cucadiagram.Display;
 import net.sourceforge.plantuml.eggs.PSystemWelcome;
-import net.sourceforge.plantuml.flashcode.FlashCodeFactory;
-import net.sourceforge.plantuml.flashcode.FlashCodeUtils;
-import net.sourceforge.plantuml.graphic.FontConfiguration;
 import net.sourceforge.plantuml.graphic.GraphicPosition;
 import net.sourceforge.plantuml.graphic.GraphicStrings;
 import net.sourceforge.plantuml.graphic.HorizontalAlignment;
-import net.sourceforge.plantuml.graphic.HtmlColor;
-import net.sourceforge.plantuml.graphic.HtmlColorSetSimple;
-import net.sourceforge.plantuml.graphic.HtmlColorSimple;
-import net.sourceforge.plantuml.graphic.HtmlColorUtils;
-import net.sourceforge.plantuml.graphic.InnerStrategy;
-import net.sourceforge.plantuml.graphic.StringBounder;
 import net.sourceforge.plantuml.graphic.TextBlock;
 import net.sourceforge.plantuml.graphic.TextBlockUtils;
-import net.sourceforge.plantuml.graphic.VerticalAlignment;
 import net.sourceforge.plantuml.svek.TextBlockBackcolored;
 import net.sourceforge.plantuml.ugraphic.ColorMapperIdentity;
 import net.sourceforge.plantuml.ugraphic.ImageBuilder;
-import net.sourceforge.plantuml.ugraphic.MinMax;
-import net.sourceforge.plantuml.ugraphic.UFont;
-import net.sourceforge.plantuml.ugraphic.UGraphic;
-import net.sourceforge.plantuml.ugraphic.UImage;
-import net.sourceforge.plantuml.ugraphic.UTranslate;
 import net.sourceforge.plantuml.ugraphic.txt.UGraphicTxt;
-import net.sourceforge.plantuml.version.LicenseInfo;
-import net.sourceforge.plantuml.version.PSystemVersion;
 
 public class PSystemError extends AbstractPSystem {
 
@@ -154,16 +129,7 @@ public class PSystemError extends AbstractPSystem {
 		} else {
 			udrawable = result;
 		}
-		final int min = (int) (System.currentTimeMillis() / 60000L) % 60;
-		if (min == 1 && LicenseInfo.retrieveNamedOrDistributorQuickIsValid() == false) {
-			udrawable = addMessagePatreon(udrawable);
-		} else if (min == 15 && LicenseInfo.retrieveNamedOrDistributorQuickIsValid() == false) {
-			udrawable = addMessageLiberapay(udrawable);
-		} else if (min == 30 && LicenseInfo.retrieveNamedOrDistributorQuickIsValid() == false) {
-			udrawable = addMessageDedication(udrawable);
-		} else if (getSource().containsIgnoreCase("arecibo")) {
-			udrawable = addMessageArecibo(udrawable);
-		}
+
 		imageBuilder.setUDrawable(udrawable);
 		final ImageData imageData = imageBuilder.writeImageTOBEMOVED(fileFormat, seed(), os);
 		((ImageDataAbstract) imageData).setStatus(FileImageData.ERROR);
@@ -177,153 +143,6 @@ public class PSystemError extends AbstractPSystem {
 	private TextBlock addWelcome(final TextBlockBackcolored result) throws IOException {
 		final TextBlockBackcolored welcome = getWelcome();
 		return TextBlockUtils.mergeTB(welcome, result, HorizontalAlignment.LEFT);
-	}
-
-	private TextBlock addMessageLiberapay(final TextBlock source) throws IOException {
-		final TextBlock message = getMessageLiberapay();
-		TextBlock result = TextBlockUtils.mergeTB(message, source, HorizontalAlignment.LEFT);
-		result = TextBlockUtils.mergeTB(result, message, HorizontalAlignment.LEFT);
-		return result;
-	}
-
-	private TextBlock addMessagePatreon(final TextBlock source) throws IOException {
-		final TextBlock message = getMessagePatreon();
-		TextBlock result = TextBlockUtils.mergeTB(message, source, HorizontalAlignment.LEFT);
-		result = TextBlockUtils.mergeTB(result, message, HorizontalAlignment.LEFT);
-		return result;
-	}
-
-	private TextBlock addMessageDedication(final TextBlock source) throws IOException {
-		final TextBlock message = getMessageDedication();
-		TextBlock result = TextBlockUtils.mergeTB(message, source, HorizontalAlignment.LEFT);
-		return result;
-	}
-
-	private TextBlock addMessageArecibo(final TextBlock source) throws IOException {
-		final UImage message = new UImage(PSystemVersion.getArecibo());
-		TextBlock result = TextBlockUtils.mergeLR(source, TextBlockUtils.fromUImage(message), VerticalAlignment.TOP);
-		return result;
-	}
-
-	private TextBlockBackcolored getMessageDedication() {
-		final FlashCodeUtils utils = FlashCodeFactory.getFlashCodeUtils();
-		final HtmlColorSimple backColor = (HtmlColorSimple) new HtmlColorSetSimple().getColorIfValid("#DFDCD3");
-
-		final BufferedImage qrcode = smaller(utils.exportFlashcode("http://plantuml.com/dedication", Color.BLACK,
-				backColor.getColor999()));
-		final Display disp = Display.create("<b>Add your own dedication into PlantUML", " ", "For just $5 per month!",
-				"Details on <i>[[http://plantuml.com/dedication]]");
-
-		final UFont font = UFont.sansSerif(14);
-		final FontConfiguration fc = new FontConfiguration(font, HtmlColorUtils.BLACK, HtmlColorUtils.BLACK, false);
-		final TextBlock text = TextBlockUtils.withMargin(
-				disp.create(fc, HorizontalAlignment.LEFT, new SpriteContainerEmpty()), 10, 0);
-		final TextBlock result;
-		if (qrcode == null) {
-			result = text;
-		} else {
-			final UImage qr = new UImage(qrcode).scaleNearestNeighbor(3);
-			result = TextBlockUtils.mergeLR(text, TextBlockUtils.fromUImage(qr), VerticalAlignment.CENTER);
-		}
-		return TextBlockUtils.addBackcolor(result, backColor);
-
-	}
-
-	private TextBlockBackcolored getMessagePatreon() {
-		final UImage message = new UImage(PSystemVersion.getTime01());
-		final Color back = new Color(message.getImage().getRGB(0, 0));
-		final HtmlColor backColor = new HtmlColorSimple(back, false);
-
-		final FlashCodeUtils utils = FlashCodeFactory.getFlashCodeUtils();
-		final BufferedImage qrcode = smaller(utils.exportFlashcode("http://plantuml.com/patreon", Color.BLACK,
-				Color.WHITE));
-
-		final int scale = 2;
-
-		final double imWidth = message.getWidth() + (qrcode == null ? 0 : qrcode.getWidth() * scale + 20);
-		final double imHeight = qrcode == null ? message.getHeight() : Math.max(message.getHeight(), qrcode.getHeight()
-				* scale + 10);
-		return new TextBlockBackcolored() {
-
-			public void drawU(UGraphic ug) {
-				if (qrcode == null) {
-					ug.apply(new UTranslate(1, 1)).draw(message);
-				} else {
-					final UImage qr = new UImage(qrcode).scaleNearestNeighbor(scale);
-					ug.apply(new UTranslate(1, (imHeight - message.getHeight()) / 2)).draw(message);
-					ug.apply(new UTranslate(1 + message.getWidth(), (imHeight - qr.getHeight()) / 2)).draw(qr);
-				}
-			}
-
-			public Rectangle2D getInnerPosition(String member, StringBounder stringBounder, InnerStrategy strategy) {
-				return null;
-			}
-
-			public Dimension2D calculateDimension(StringBounder stringBounder) {
-				return new Dimension2DDouble(imWidth + 1, imHeight + 1);
-			}
-
-			public MinMax getMinMax(StringBounder stringBounder) {
-				return MinMax.fromMax(imWidth + 1, imHeight + 1);
-			}
-
-			public HtmlColor getBackcolor() {
-				return backColor;
-			}
-		};
-
-	}
-
-	private TextBlockBackcolored getMessageLiberapay() {
-		final UImage message = new UImage(PSystemVersion.getTime15());
-		final Color back = new Color(message.getImage().getRGB(0, 0));
-		final HtmlColor backColor = new HtmlColorSimple(back, false);
-
-		final FlashCodeUtils utils = FlashCodeFactory.getFlashCodeUtils();
-		final BufferedImage qrcode = smaller(utils.exportFlashcode("http://plantuml.com/lp", Color.BLACK, Color.WHITE));
-
-		final int scale = 2;
-
-		final double imWidth = message.getWidth() + (qrcode == null ? 0 : qrcode.getWidth() * scale + 20);
-		final double imHeight = qrcode == null ? message.getHeight() : Math.max(message.getHeight(), qrcode.getHeight()
-				* scale + 10);
-		return new TextBlockBackcolored() {
-
-			public void drawU(UGraphic ug) {
-				if (qrcode == null) {
-					ug.apply(new UTranslate(1, 1)).draw(message);
-				} else {
-					final UImage qr = new UImage(qrcode).scaleNearestNeighbor(scale);
-					ug.apply(new UTranslate(1, (imHeight - message.getHeight()) / 2)).draw(message);
-					ug.apply(new UTranslate(1 + message.getWidth(), (imHeight - qr.getHeight()) / 2)).draw(qr);
-				}
-			}
-
-			public Rectangle2D getInnerPosition(String member, StringBounder stringBounder, InnerStrategy strategy) {
-				return null;
-			}
-
-			public Dimension2D calculateDimension(StringBounder stringBounder) {
-				return new Dimension2DDouble(imWidth + 1, imHeight + 1);
-			}
-
-			public MinMax getMinMax(StringBounder stringBounder) {
-				return MinMax.fromMax(imWidth + 1, imHeight + 1);
-			}
-
-			public HtmlColor getBackcolor() {
-				return backColor;
-			}
-		};
-
-	}
-
-	private BufferedImage smaller(BufferedImage im) {
-		if (im == null) {
-			return null;
-		}
-		final int nb = 1;
-		return im.getSubimage(nb, nb, im.getWidth() - 2 * nb, im.getHeight() - 2 * nb);
 	}
 
 	private List<String> getTextStrings() {
